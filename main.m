ccec3b7383e00b635f5845aa8ec0a018c8ecf45c
Acciaro Gennaro Daniele <acciarogennaro@gmail.com>
2023-06-30 12:57:33 +0200

    Avanzamenti

--- main.m
+++ main.m
@@ -1,21 +1,15 @@
 seed = 42;
-filename = "graphs/net10_8_1.dmx";
+filename = "graphs/net8_8_1.dmx";
 [E, D, b] = utility_read_matrix(filename, seed);
 
-dim = size(D,1)+size(E,1);
 
 starting_point  = b;
-max_iterations  = dim;
 threshold       = 1e-10;
 reorth_flag     = true;
+P = NaN;
 tic;
-[x, e, r_norm] = our_gmres(D, E, b, starting_point, max_iterations, threshold, reorth_flag);
+[x, e, r_norm] = our_gmres(D, E, P, b, starting_point, threshold, true);
 toc;
 
 disp(r_norm);
 
-tic;
-[x, e, r_norm] = our_gmres(D, E, b, starting_point, max_iterations, threshold, false);
-toc;
-
-disp(r_norm);
