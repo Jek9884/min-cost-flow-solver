6d76436e4131c0e59bac021c8874b2029faf5c00
Acciaro Gennaro Daniele <acciarogennaro@gmail.com>
2023-08-08 10:20:47 +0200

    Fix

--- test/A1_tests/A1_exp3_test_comparisons_with_native_methods/test_comparison_native_methods.m
+++ test/A1_tests/A1_exp3_test_comparisons_with_native_methods/test_comparison_native_methods.m
@@ -3,7 +3,7 @@ experiment_title = "exp_3";
 addpath(path_to_root)
 format long;
 seed = 42;
-filenames = [path_to_root+"graphs/net8_8_3.dmx", path_to_root+"graphs/net10_8_3.dmx", path_to_root+"graphs/net12_8_3.dmx";
+filenames = [path_to_root+"graphs/net8_8_3.dmx", path_to_root+"graphs/net10_8_3.dmx", path_to_root+"graphs/net12_8_3.dmx"];
 threshold = 1e-10;
 debug = false;
 
